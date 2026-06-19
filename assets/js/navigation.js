e6b8aa102b0955c339752f299981335d3fa79773
dovaya <dovaya.creations@gmail.com>
2026-06-19 21:47:20 +0200

    improve JS script import

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,59 +1,50 @@
-/**
- * @file
- * A JavaScript file for the navigation list dropdown trigger.
- */
+// Select all triggers and buttons
+const triggers = document.querySelectorAll(".nav__sub-list-trigger");
 
-(function () {
-  "use strict";
+// Function to close all triggers
+const closeAllTriggers = () => {
+  triggers.forEach((trigger) => {
+    const menu = trigger.parentElement;
+    menu.classList.remove("open");
+    menu.setAttribute("aria-expanded", "false");
+  });
+};
 
-  // Select all triggers and buttons
-  const triggers = document.querySelectorAll(".nav__sub-list-trigger");
+// Add event listeners to each trigger button
+triggers.forEach((trigger) => {
+  trigger.addEventListener("click", function (event) {
+    triggers.forEach((t) => {
+      if (t !== trigger) {
+        const m = t.parentElement;
+        m.classList.remove("open");
+        m.setAttribute("aria-expanded", "false");
+      }
+    });
 
-  // Function to close all triggers
-  const closeAllTriggers = () => {
-    triggers.forEach((trigger) => {
-      const menu = trigger.parentElement;
+    const menu = trigger.parentElement;
+
+    // Toggle the current trigger
+    if (trigger.classList.contains("open")) {
       menu.classList.remove("open");
       menu.setAttribute("aria-expanded", "false");
-    });
-  };
-
-  // Add event listeners to each trigger button
-  triggers.forEach((trigger) => {
-    trigger.addEventListener("click", function (event) {
-      triggers.forEach((t) => {
-        if (t !== trigger) {
-          const m = t.parentElement;
-          m.classList.remove("open");
-          m.setAttribute("aria-expanded", "false");
-        }
-      });
-
-      const menu = trigger.parentElement;
-
-      // Toggle the current trigger
-      if (trigger.classList.contains("open")) {
-        menu.classList.remove("open");
-        menu.setAttribute("aria-expanded", "false");
-      } else {
-        menu.classList.toggle("open");
-        menu.setAttribute("aria-expanded", "true");
-      }
-
-      // Stop propagation to prevent document click handler
-      event.stopPropagation();
-    });
-  });
-
-  // Close triggers when clicking outside
-  document.addEventListener("click", function (e) {
-    closeAllTriggers();
-  });
-
-  // Close triggers when escaping
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape") {
-      closeAllTriggers();
+    } else {
+      menu.classList.toggle("open");
+      menu.setAttribute("aria-expanded", "true");
     }
+
+    // Stop propagation to prevent document click handler
+    event.stopPropagation();
   });
-})();
+});
+
+// Close triggers when clicking outside
+document.addEventListener("click", function (e) {
+  closeAllTriggers();
+});
+
+// Close triggers when escaping
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    closeAllTriggers();
+  }
+});
--- assets/js/sidebar-toggle.js
+++ assets/js/sidebar-toggle.js
@@ -1,63 +1,54 @@
-/**
- * @file
- * A JavaScript file for the sidebar toggle.
- */
+const toggle = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".container-sidebar");
+const main = document.querySelector(".container-main");
 
-(function () {
-  "use strict";
+function navOpen() {
+  sidebar.classList.add("open");
+  sidebar.setAttribute("aria-hidden", "false");
+  toggle.classList.add("open");
+  toggle.setAttribute("aria-expanded", "true");
+  document.body.addEventListener("keydown", closeOnESC);
+}
 
-  const toggle = document.querySelector(".sidebar-toggle");
-  const sidebar = document.querySelector(".container-sidebar");
-  const main = document.querySelector(".container-main");
+function navClose() {
+  sidebar.classList.remove("open");
+  sidebar.setAttribute("aria-hidden", "true");
+  toggle.classList.remove("open");
+  toggle.setAttribute("aria-expanded", "false");
+  document.body.removeEventListener("keydown", closeOnESC);
+}
 
-  function navOpen() {
-    sidebar.classList.add("open");
-    sidebar.setAttribute("aria-hidden", "false");
-    toggle.classList.add("open");
-    toggle.setAttribute("aria-expanded", "true");
-    document.body.addEventListener("keydown", closeOnESC);
+function closeOnESC(e) {
+  if (e.key === "Escape") {
+    navClose();
   }
+}
 
-  function navClose() {
-    sidebar.classList.remove("open");
-    sidebar.setAttribute("aria-hidden", "true");
-    toggle.classList.remove("open");
-    toggle.setAttribute("aria-expanded", "false");
-    document.body.removeEventListener("keydown", closeOnESC);
+toggle.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (sidebar.classList.contains("open")) {
+    navClose();
+  } else {
+    navOpen();
   }
+});
 
-  function closeOnESC(e) {
-    if (e.key === "Escape") {
-      navClose();
-    }
+main.addEventListener("click", function (e) {
+  if (sidebar.classList.contains("open")) {
+    navClose();
   }
+});
 
-  toggle.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (sidebar.classList.contains("open")) {
-      navClose();
-    } else {
-      navOpen();
-    }
+// Close the nav sidebar after click (needed for anchor links).
+const links = document.querySelectorAll(".container-sidebar a");
+links.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    navClose();
   });
+});
 
-  main.addEventListener("click", function (e) {
-    if (sidebar.classList.contains("open")) {
-      navClose();
-    }
-  });
-
-  // Close the nav sidebar after click (needed for anchor links).
-  const links = document.querySelectorAll(".container-sidebar a");
-  links.forEach(function (link) {
-    link.addEventListener("click", function (e) {
-      navClose();
-    });
-  });
-
-  // Move focus back to button efter user tab out of last link.
-  if (links.length) {
-    const lastlink = [].slice.call(links).pop();
-    lastlink.addEventListener("blur", () => toggle.focus());
-  }
-})();
+// Move focus back to button efter user tab out of last link.
+if (links.length) {
+  const lastlink = [].slice.call(links).pop();
+  lastlink.addEventListener("blur", () => toggle.focus());
+}
