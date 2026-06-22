76a4570159719b8e7e611ba0ddf2cb42c836814c
dovaya <dovaya.creations@gmail.com>
2026-06-22 17:47:23 +0200

    refactor navigation logic, refactor sidebar styling, improve language selection, improve sidebar design

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,50 +1,17 @@
 // Select all triggers and buttons
-const triggers = document.querySelectorAll(".nav__sub-list-trigger");
-
-// Function to close all triggers
-const closeAllTriggers = () => {
-  triggers.forEach((trigger) => {
-    const menu = trigger.parentElement;
-    menu.classList.remove("open");
-    menu.setAttribute("aria-expanded", "false");
-  });
-};
+const triggers = document.querySelectorAll(".sublist-trigger");
 
 // Add event listeners to each trigger button
 triggers.forEach((trigger) => {
   trigger.addEventListener("click", function (event) {
-    triggers.forEach((t) => {
-      if (t !== trigger) {
-        const m = t.parentElement;
-        m.classList.remove("open");
-        m.setAttribute("aria-expanded", "false");
-      }
-    });
 
-    const menu = trigger.parentElement;
+    const menu = trigger.nextElementSibling;
+    const isOpen = (menu.getAttribute('aria-expanded')) === "true";
 
-    // Toggle the current trigger
-    if (trigger.classList.contains("open")) {
-      menu.classList.remove("open");
-      menu.setAttribute("aria-expanded", "false");
-    } else {
-      menu.classList.toggle("open");
-      menu.setAttribute("aria-expanded", "true");
-    }
-
-    // Stop propagation to prevent document click handler
-    event.stopPropagation();
+    menu.setAttribute(
+      "aria-expanded",
+      isOpen ? "false" : "true"
+    );
   });
 });
 
-// Close triggers when clicking outside
-document.addEventListener("click", function (e) {
-  closeAllTriggers();
-});
-
-// Close triggers when escaping
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    closeAllTriggers();
-  }
-});
